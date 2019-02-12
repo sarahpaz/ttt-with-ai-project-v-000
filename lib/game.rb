f3f193ea5c673e7fad7b88d2df7a5ac993b8e13c
Sarah Paz <sarah.paz@gmail.com>
2019-02-12 04:17:14 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -62,25 +62,4 @@ class Game
       puts "Congratulations #{winner}!"
     end
   end
-
-  def self.start
-    puts "Welcome to Tic Tac Toe!"
-    puts "Would you like to play '0', '1', or '2' player?"
-    input = gets.strip
-
-    if input == "1"
-      puts "Do you want to go first? [y/ n]"
-      if gets.strip == "y"
-        game = Game.new(Players::Human.new("X"), Players::Computer.new("O"), Board.new).play
-      else
-        game = Game.new(Players::Computer.new("X"), Players::Human.new("O"), Board.new).play
-      end
-    elsif input == "0"
-      game = Game.new(Players::Computer.new("X"), Players::Computer.new("O"), Board.new).play
-    elsif input == "2"
-      game = Game.new(Players::Human.new("X"), Players::Human.new("O"), Board.new).play
-    end
-    puts "Would like to play again? [y/ n]"
-    game.start unless gets.strip "n"
-  end
 end
